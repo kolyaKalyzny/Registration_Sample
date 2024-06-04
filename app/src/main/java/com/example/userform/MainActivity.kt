374705b9d80c41f14ede55a5d9a882ef10de9b03
Альберт Терликпаев <albert.terlikpayev@freedompay.kz>
2024-06-04 17:10:49 +0600

    user form ui only

--- app/src/main/java/com/example/userform/MainActivity.kt
+++ app/src/main/java/com/example/userform/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,8 +25,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.userform.ui.theme.UserFormTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,16 +55,18 @@ fun UserForm() {
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 45.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .size(width = 600.dp, height = 500.dp)
+            .padding(all = 45.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
     )
     {
 
         TextField(
             value = textName,
             onValueChange = { textName = it},
-            label = { Text("Name")}
+            label = { Text("Name")},
+            modifier = Modifier.width(400.dp)
         )
 
         Spacer(modifier = Modifier.size(10.dp))
@@ -65,7 +74,10 @@ fun UserForm() {
         TextField(
             value = textPhoneNumber,
             onValueChange = { textPhoneNumber = it},
-            label = { Text("Phone Number")}
+            label = { Text("Phone Number")},
+            modifier = Modifier.width(400.dp)
+
+
         )
 
         Spacer(modifier = Modifier.size(10.dp))
@@ -73,8 +85,19 @@ fun UserForm() {
         TextField(
             value = textPhoneNumber,
             onValueChange = { textPhoneNumber = it},
-            label = { Text("Something")}
+            label = { Text("Something")},
+            modifier = Modifier.width(400.dp)
+
         )
+
+        Spacer(modifier = Modifier.size(30.dp))
+
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier
+                .size(width = 120.dp, height = 45.dp)
+        ) {
+            Text("Submit", fontSize = 18.sp,)
+        }
     }
 
 }
